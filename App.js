3b186576dd22a9436065046a96b1a34f12d155db
loloof64 <laurent bernabe>
2017-10-21 19:39:31 +0200

    Board size adjusts to device screen.

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Dimensions } from 'react-native';
 import styled from 'styled-components/native';
 
 import ChessBoard from './components/chessboard';
@@ -8,6 +9,7 @@ const TopPanel = styled.View`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  background-color: #CCFFCC;
 `;
 
 const Button = styled.TouchableHighlight`
@@ -24,10 +26,17 @@ const ButtonText = styled.Text`
 
 export default class App extends Component {
 
+  constructor() {
+    super();
+    const { height, width } = Dimensions.get('window');
+    const minDimension = width < height ? width : height;
+    this.cellsSize = parseInt(minDimension * 0.1);
+  }
+
   render() {
     return (
       <TopPanel>
-        <ChessBoard cellsSize={40} />
+        <ChessBoard cellsSize={this.cellsSize} />
       </TopPanel>
     );
   }
