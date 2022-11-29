dee4913452a351efa924c3ea0a6148ab2cf30a74
loloof64 <laurent.bernabe@gmail.com>
2022-11-29 23:52:10 +0100

    added license

--- lib/screens/game_page.dart
+++ lib/screens/game_page.dart
@@ -1,3 +1,21 @@
+/*
+    ChessPositionGenerator : generate a chess position from your criterias.
+    Copyright (C) 2022  Laurent Bernabe
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 import 'package:chess/chess.dart' as chess;
 import 'package:chess_vectors_flutter/chess_vectors_flutter.dart';
 import 'package:flutter/material.dart';
